22acd8c4a288dab8fa4ceef677e230a4d085f2a8
shawon <shawonislam121009@gmail.com>
2021-09-11 16:44:45 +0600

    added the order api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const ObjectId = require('mongodb').ObjectId
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
@@ -17,13 +18,44 @@ const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://tshirt-user:ZEScgQTWMuC5X4i@cluster0.lhztw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const tshirtCollections = client.db("comfyTshirts").collection("tshirts");
-    console.log('database connected successfully')
+    const tshirtsCollection = client.db("comfyTshirts").collection("tshirts");
+    const ordersCollection = client.db("comfyTshirts").collection("orders");
+
+    app.get('/products', (req, res) => {
+
+        tshirtsCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+    app.post('/addProduct', (req, res) => {
+        const newProduct = req.body
+        tshirtsCollection.insertOne(newProduct)
+            .then(result => {
+                res.send(result.acknowledged)
+            })
+    })
+
+    app.post('/addOrder', (req, res) => {
+        const newOrder = req.body
+        console.log(newOrder)
+        ordersCollection.insertOne(newOrder)
+            .then(result => {
+                res.send(result.acknowledged)
+            })
+    })
+    app.delete('/deleteProduct', (req, res) => {
+        const _id = req.body
+        tshirtsCollection.deleteOne({ _id: ObjectId(_id._id) })
+            .then(result => {
+                console.log(result)
+                res.send(result.deletedCount > 0)
+            })
+    })
+
 });
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 
 app.listen(port)
